eb5a839b3cdc63d02b49a8037b4cc4ebcf8e3b3c
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2024-02-03 14:07:39 +0700

    Update streamlit_gbooks.py

--- streamlit_gbooks.py
+++ streamlit_gbooks.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # Configure
 st.set_page_config(
-    page_title=":book: Google Book Dataset Analysis :book:",
+    page_title=:book:"Google Book Dataset Analysis":book:,
     layout="wide"
 )
 st.title('Googe Book Dataset Analysis')
